dccf4f6892bc4351587404b413a464fe78b598b7
Frédéric Mangano <fred.mangano@gmail.com>
2012-09-02 15:40:34 +0200

    day_of_week: no debugging

--- calf.c
+++ calf.c
@@ -2,20 +2,17 @@
 #include <stdio.h>
 #include <time.h>
 
-void day_of_week(int year, int month, int day){
+int day_of_week(int year, int month, int day){
     struct tm date;
     date.tm_hour = date.tm_min = date.tm_sec = 0;
     date.tm_year = year-1900;
     date.tm_mon = month-1;
     date.tm_mday = day;
     mktime(&date);
-    char buf[256];
-    strftime(buf, 256, "%F", &date);
-    printf("Date: %s\n", buf);
-    printf("Day of week: %d\n", date.tm_wday);
+    return date.tm_wday;
 }
 
 int main(int argc, char **argv){
-    day_of_week(2012, 9, 2);
+    printf("Day of week: %d\n", day_of_week(2012, 9, 2));
     return EXIT_SUCCESS;
 }

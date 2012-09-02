870b79eeff9b163058919f3d883d0051aa64dfc1
Frédéric Mangano <fred.mangano@gmail.com>
2012-09-02 15:48:27 +0200

    is_leap_year, days_for_month

--- calf.c
+++ calf.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #include <time.h>
 
+int is_leap_year(int year){
+    if(year % 400 == 0) return 1;
+    if(year % 100 == 0) return 0;
+    if(year % 4 == 0) return 1;
+    return 0;
+}
+
+int days_for_month(int year, int month){
+    if(month == 2) return is_leap_year(year) ? 29 : 28;
+    if(month == 4 || month == 6 || month == 9 || month == 11) return 30;
+    return 31;
+}
+
 int day_of_week(int year, int month, int day){
     struct tm date;
     date.tm_hour = date.tm_min = date.tm_sec = 0;

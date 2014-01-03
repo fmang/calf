a49ac6d00faeeb820f61fdeaad5a35227495daa9
Frédéric Mangano <fmang@mg0.fr>
2014-01-03 13:29:16 +0100

    screw html calendars

--- html.c
+++ html.c
@@ -63,93 +63,6 @@ static char *concat(const char *s1, const char *s2)
 }
 
 /*******************************************************************************
- * Date-related functions
- * Calendar generation
- */
-
-static int is_leap_year(struct tm *date)
-{
-	if (date->tm_year % 400 == 0)
-		return 1;
-	if (date->tm_year % 100 == 0)
-		return 0;
-	if (date->tm_year % 4 == 0)
-		return 1;
-	return 0;
-}
-
-static int month_length(struct tm *date)
-{
-	int month = date->tm_mon + 1;
-	if (month == 2)
-		return is_leap_year(date) ? 29 : 28;
-	if (month == 4 || month == 6 || month == 9 || month == 11)
-		return 30;
-	return 31;
-}
-
-static void format_calendar(struct context *ctx, struct calendar *cal, int month)
-{
-	struct tm date = {
-		.tm_mday = 1,
-		.tm_mon  = month,
-		.tm_year = cal->year,
-	};
-	if (mktime(&date) == -1)
-		return;
-	int current_year = cal->year == ctx->date.tm_year;
-	int day = 0;
-	if (current_year && month == ctx->date.tm_mon)
-		day = ctx->date.tm_mday;
-	int last_day = month_length(&date);
-	printf(snip_calendar_header, ft(snip_date_calendar_title, &date));
-	while (date.tm_mday <= last_day) {
-		if (date.tm_wday == 1 || date.tm_mday == 1)
-			printf(snip_calendar_week);
-		if (date.tm_mday == 1) { /* pad */
-			int dow = (date.tm_wday + 6) % 7;
-			for (int i = 0; i < dow; i++)
-				put(snip_calendar_empty_cell);
-		}
-		int today = date.tm_mday == day;
-		int linked = cal->months[month] & (1 << (date.tm_mday - 1));
-		printf(
-			today  ? snip_calendar_current_day :
-			linked ? snip_calendar_linked_day  :
-			snip_calendar_regular_day,
-			ft(snip_date_calendar_day, &date),
-			ctx->base_uri, ft("%F", &date)
-		);
-		if (date.tm_wday == 0)
-			printf(snip_calendar_week_end);
-		/* increment */
-		date.tm_mday++;
-		date.tm_wday = (date.tm_wday + 1) % 7;
-		date.tm_yday++;
-	}
-	if (date.tm_wday != 0) { /* pad */
-		int dow = date.tm_wday - 1;
-		for (int i = 0; i < 6 - dow; i++)
-			put(snip_calendar_empty_cell);
-		printf(snip_calendar_week_end);
-	}
-	printf(snip_calendar_footer);
-}
-
-static void html_calendars(struct context *ctx)
-{
-	put(snip_calendars_header);
-	for (struct calendar *cal = ctx->calendars; cal; cal = cal->next) {
-		for (int i = 0; i < 12; i++) {
-			if (cal->months[i] == 0)
-				continue;
-			format_calendar(ctx, cal, i);
-		}
-	}
-	put(snip_calendars_footer);
-}
-
-/*******************************************************************************
  * Listing
  */
 
@@ -213,7 +126,6 @@ static void html_200(struct context *ctx)
 	    "\n"
 	);
 	printf(snip_header, ft(snip_date_title, &ctx->date), ctx->title, ctx->base_uri);
-	html_calendars(ctx);
 	html_listing(ctx);
 	put(snip_footer);
 	obstack_free(&ob, NULL);
